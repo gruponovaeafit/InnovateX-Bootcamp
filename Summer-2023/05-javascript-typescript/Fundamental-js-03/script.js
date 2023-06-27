a4debb8fa3c8d524a00c1b556a8b7ddf7b5593d0
SaraCmque <saramanrique2016@gmail.com>
2023-06-27 14:57:36 -0500

    add ventana modal

--- Summer-2023/05-javascript-typescript/Fundamental-js-03/script.js
+++ Summer-2023/05-javascript-typescript/Fundamental-js-03/script.js
@@ -1,14 +1,24 @@
 const submit = document.querySelector("#submit");
 const inputs = document.querySelectorAll("input");
-let persona = []; //me muestra los datos de persona
+let persona = {}; //me muestra los datos de persona
 submit.addEventListener("click", function () {
   let estavacio = false;
   inputs.forEach((input) => {
     let i = input.value.trim();
-    persona.push(i);
+    // persona.push(i);
     // console.log(i);
     if (i.length === 0) {
       estavacio = true;
+    }
+
+    if (input.id === "name") {
+      persona.name = i;
+    } else if (input.id === "id") {
+      persona.id = i;
+    } else if (input.id === "carrera") {
+      persona.carrera = i;
+    } else if (input.id === "correo") {
+      persona.correo = i;
     }
   });
   if (estavacio) {
