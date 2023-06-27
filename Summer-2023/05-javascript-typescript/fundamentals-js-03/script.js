4dff9a58caa6848fd9ae06a18690cfc067dccb0a
endi <endifranco12@gmail.com>
2023-06-27 14:02:19 -0500

    Add several objects he user click enviar button

--- Summer-2023/05-javascript-typescript/fundamentals-js-03/script.js
+++ Summer-2023/05-javascript-typescript/fundamentals-js-03/script.js
@@ -8,10 +8,20 @@ const submit = document.querySelector("#submit");
 const inputs = document.querySelectorAll("input");
 let persona = [];
 
-console.log(inputs);
+class person {
+    constructor(name, id, degree, email) {
+        this.name = name;
+        this.id = id;
+        this.degree = degree;
+        this.email = email;
+    }
+}
+
+let listaObjetos = [];
 
 submit.addEventListener("click", function() {
     let estaVacio;
+    
     inputs.forEach((input) => {
         let i = input.value.trim();
         persona.push(i);
@@ -19,12 +29,20 @@ submit.addEventListener("click", function() {
         if(i.length === 0) {
             estaVacio = true;
         }
-    });
+        
+    }
+    );
+    let personaEjemplo = new person(persona[0], persona[1], persona[2], persona[3]);
+    console.log(personaEjemplo);
+    listaObjetos.push(personaEjemplo);
+    console.log(listaObjetos);
+
     if (estaVacio) {
         alert('Alert')
     } else {
         alert('Enviado')
     }
-    
 })
+
+
 
